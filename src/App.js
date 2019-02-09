91741fe80e0f46c3733f2f1746566b6fef1cbf48
Bora Son <btson310@gmail.com>
2019-02-09 14:14:04 -0700

    search page and search button state functionality added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Shelf from './components/Shelf.js';
+import Search from './components/Search.js';
+import SearchButton from './components/SearchButton.js';
 
 class BooksApp extends React.Component {
-  state = {
+  constructor(props) {
+    super(props);
+    this.state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    }
   }
+
+
+updateSearchPageStatus = (bool) => {
+  this.setState({showSearchPage: bool});
+};
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <Search />
+          <Search showSearchPage={this.state.showSearchPage} closeSearch={this.updateSearchPageStatus} />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -29,9 +39,7 @@ class BooksApp extends React.Component {
                 <Shelf />
               </div>
             </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
+            <SearchButton openSearch={this.updateSearchPageStatus} />
           </div>
         )}
       </div>
